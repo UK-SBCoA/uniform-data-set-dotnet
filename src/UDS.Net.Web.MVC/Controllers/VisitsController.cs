366321edfdcc022c8fe74eb717500e7784888173
Casey D Spires <11319139+casey-spires@users.noreply.github.com>
2022-10-13 15:41:53 -0400

    Fixes code formatting in controller

--- src/UDS.Net.Web.MVC/Controllers/VisitsController.cs
+++ src/UDS.Net.Web.MVC/Controllers/VisitsController.cs
@@ -2,18 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;using UDS.Net.Services;
+using Microsoft.AspNetCore.Mvc;
+using UDS.Net.Services;
 using UDS.Net.Web.MVC;
 
 namespace UDS.Net.Web.MVC.Controllers
 {
-    public class VisitsController: Controller
+    public class VisitsController : Controller
     {
         private readonly IVisitService _visitService;
-        public VisitsController(IVisitService visitService) {
+        public VisitsController(IVisitService visitService)
+        {
             _visitService = visitService;
         }
-        public async Task<IActionResult> Index() {
+        public async Task<IActionResult> Index()
+        {
             var visits = await _visitService.List(User.Identity.Name);
             return View(visits);
         }
