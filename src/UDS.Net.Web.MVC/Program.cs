88e43281af6e06c5cebd0564f987a7b9282f474b
Ashley Wilson <ashleybot@users.noreply.github.com>
2022-11-08 08:58:09 -0500

    Squashed commit of the following:
    commit 59c5794c3873c77129c86b88ca76cf338c3eb30e
    Author: Ashley Wilson <ashleybot@users.noreply.github.com>
    Date:   Fri Oct 14 14:12:39 2022 -0400
    
        Updates with correct base URI for api
    
    commit ca27f71e11ff49416c1ce626198253a72b4a57c6
    Author: Casey D Spires <11319139+casey-spires@users.noreply.github.com>
    Date:   Fri Oct 14 13:54:38 2022 -0400
    
        Moves port to match order of other services
    
    commit ee63d50dd0f5ff7235f068cb1cc1897c393a8745
    Author: Casey D Spires <11319139+casey-spires@users.noreply.github.com>
    Date:   Fri Oct 14 13:49:27 2022 -0400
    
        Moves properties from override to main docker-compose file
    
    commit fe0f218dd30f187bdb769f867f38b573bde715df
    Author: Casey D Spires <11319139+casey-spires@users.noreply.github.com>
    Date:   Fri Oct 14 11:35:17 2022 -0400
    
        Removes unused docker directory
    
    commit a6656b25fb62c7062896eedb8659fb7470a6bb41
    Author: Ashley Wilson <ashleybot@users.noreply.github.com>
    Date:   Fri Oct 14 09:29:30 2022 -0400
    
        Reformatted whitespace
    
    commit 366321edfdcc022c8fe74eb717500e7784888173
    Author: Casey D Spires <11319139+casey-spires@users.noreply.github.com>
    Date:   Thu Oct 13 15:41:53 2022 -0400
    
        Fixes code formatting in controller
    
    commit 0de052209d674180a6c67766d9ff39a318490fd3
    Author: Casey D Spires <11319139+casey-spires@users.noreply.github.com>
    Date:   Thu Oct 13 14:57:40 2022 -0400
    
        Adds API call to visits list
    
    commit 25865a1b5c2b82d06acd0dfadccb979c99677a36
    Author: Casey D Spires <11319139+casey-spires@users.noreply.github.com>
    Date:   Thu Oct 13 14:57:13 2022 -0400
    
        Adds docker compose project files
    
    commit 83849b0c7608b3caac47f222c60b8a8b27e966f8
    Author: Casey D Spires <11319139+casey-spires@users.noreply.github.com>
    Date:   Thu Oct 13 14:56:10 2022 -0400
    
        Updates default app settings files
    
    commit e95adca9eeb57d5867ec01bc0ef92a382f1d0ed6
    Author: Casey D Spires <11319139+casey-spires@users.noreply.github.com>
    Date:   Thu Oct 13 14:55:54 2022 -0400
    
        Updates launch settings
    
    commit 3139e0ece716dc37ce4e9ed2cc653a99e0ac8413
    Author: Casey D Spires <11319139+casey-spires@users.noreply.github.com>
    Date:   Thu Oct 13 14:55:40 2022 -0400
    
        Adds automatic migrations and disables ssl redirection for api
    
        SSL is being disabled for the API due to the docker networking being isolated and requiring TLS to communicate between containers
    
    commit bcb555f3a805fd28de1fe17309f1d71ead2a680e
    Author: Casey D Spires <11319139+casey-spires@users.noreply.github.com>
    Date:   Thu Oct 13 14:53:39 2022 -0400
    
        Adds docker files for visual studio development environment
    
    commit c3e0116f5db16f8c3fd09631bbe3369481c70b21
    Author: Casey D Spires <11319139+casey-spires@users.noreply.github.com>
    Date:   Mon Oct 3 16:10:27 2022 -0400
    
        Removes settings that removes appsettings files from build
    
    commit bd3221f1dd8cba609d16633450876ae9d7b2962d
    Author: Casey D Spires <11319139+casey-spires@users.noreply.github.com>
    Date:   Mon Oct 3 16:09:46 2022 -0400
    
        Adds launchsettings.json to API
    
    commit ddf05ee8a88897141a9ca7f1e34d62f6c6f313ef
    Author: Casey D Spires <11319139+casey-spires@users.noreply.github.com>
    Date:   Mon Oct 3 16:09:28 2022 -0400
    
        Remove sqllite files from project
    
    commit 786da3acf5a6afd48f5982e181c75ad2d13a9676
    Author: Casey D Spires <11319139+casey-spires@users.noreply.github.com>
    Date:   Mon Oct 3 16:09:10 2022 -0400
    
        Adds docker files and compose to project
    
    commit 37c5741cb42d732e430290cc00104482b6ef90a8
    Author: Casey D Spires <11319139+casey-spires@users.noreply.github.com>
    Date:   Mon Oct 3 16:08:41 2022 -0400
    
        Adds dev environment to .gitignore
    
    commit 9c05e2301020913e59f2f308a285806d00ca02a1
    Author: Casey D Spires <11319139+casey-spires@users.noreply.github.com>
    Date:   Mon Oct 3 16:08:16 2022 -0400
    
        Removes devcontainer from project

--- src/UDS.Net.Web.MVC/Program.cs
+++ src/UDS.Net.Web.MVC/Program.cs
@@ -43,6 +43,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        db.Database.Migrate();
+    }
 }
 else
 {
