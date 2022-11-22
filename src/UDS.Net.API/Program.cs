d96a9404746ff4bb86d3303633a39bf332d3af69
Casey D Spires <11319139+casey-spires@users.noreply.github.com>
2022-11-22 09:20:42 -0500

    Updates program.cs to use sql server instead of sqllite

--- src/UDS.Net.API/Program.cs
+++ src/UDS.Net.API/Program.cs
@@ -1,14 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using UDS.Net.API.Data;
 using UDS.Net.API.HealthChecks;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<ApiDbContext>(options =>
-    options.UseSqlite(connectionString));
+    options.UseSqlServer(connectionString));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
