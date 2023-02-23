2b4be2f37bf21e735c7d7c99afabdba5ba8e773f
Ashley Wilson <ashleybot@users.noreply.github.com>
2023-02-23 15:17:51 -0500

    Organize stimulus, responsive main menu

--- src/UDS.Net.Forms/wwwroot/js/stimulus_controllers.js
+++ src/UDS.Net.Forms/wwwroot/js/stimulus_controllers.js
@@ -1,44 +1,10 @@
-﻿import { Application, Controller } from "https://unpkg.com/@hotwired/stimulus/dist/stimulus.js"
+﻿import { Application } from "@hotwired/stimulus"
+/* Import custom stimulus controllers */
+import Dropdown from "./js_controllers/dropdown_controller.js"
+import MobileMenu from "./js_controllers/mobilemenu_controller.js"
+
 window.Stimulus = Application.start()
 
-Stimulus.register("dropdown", class extends Controller {
-    static targets = [ 'menu', 'button' ]
-    static values = { open: Boolean }
-
-    toggle() {
-        this.openValue = !this.openValue;
-    }
-
-    hide(event) {
-        if (this.element.contains(event.target) === false && this.openValue) {
-            this.openValue = false
-        }
-    }
-
-    // subscribe to value event change
-    openValueChanged() {
-        if (this.openValue) {
-            this._show();
-        } else {
-            this._hide();
-        }
-    }
-
-    _show() {
-        if (this.hasMenuTarget) {
-            this.menuTarget.classList.remove('scale-95');
-            this.menuTarget.classList.add('scale-100');
-            this.menuTarget.classList.remove('opacity-0');
-            this.menuTarget.classList.add('opacity-100');
-        }
-    }
-
-    _hide() {
-        if (this.hasMenuTarget) {
-            this.menuTarget.classList.remove('scale-100');
-            this.menuTarget.classList.add('scale-95');
-            this.menuTarget.classList.add('opacity-0');
-            this.menuTarget.classList.remove('opacity-100');
-        }
-    }
-})
+/* Register custom stimulus controllers*/
+Stimulus.register("dropdown", Dropdown)
+Stimulus.register("mobilemenu", MobileMenu)
