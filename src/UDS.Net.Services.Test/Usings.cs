b7151954afead5e28aa6c343e5d2c33ffd0a817a
Ashley Wilson <ashleybot@users.noreply.github.com>
2023-05-23 11:01:27 -0400

    Replace with published docker images

--- src/UDS.Net.Services.Test/Usings.cs
+++
@@ -1,4 +0,0 @@
-﻿global using Microsoft.VisualStudio.TestTools.UnitTesting;
-global using UDS.Net.Dto;
-global using UDS.Net.Services.DomainModels;
-global using UDS.Net.Services.Extensions;
--- src/UDS.Net.Web.MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UDS.Net.Web.MVC.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
